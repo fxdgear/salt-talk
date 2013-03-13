eb3e8be2085d2acba109fc071b82370e1dbb00ad
Nick Lang <nick.lang@gmail.com>
2013-03-13 02:57:55 +0000

    moving stuff around

--- _modules/init_site.py
+++
@@ -1,8 +0,0 @@
-def syncdb():
-    settings = "autoroute.prod"
-    command = "syncdb"
-    bin_env = "/home/django/treadhub.com"
-    pythonpath = "/home/django/treadhub.com/src/leadville/"
-
-    return __salt__['django.command'](
-        settings, command, bin_env, pythonpath)
---
+++ states/_modules/mysite.py
@@ -0,0 +1,29 @@
+def syncdb():
+    command = "syncdb"
+    SETTINGS = "autoroute.prod"
+    BIN_ENV = "/home/django/treadhub.com"
+    PYTHONPATH = "/home/django/treadhub.com/src/leadville/"
+
+    return __salt__['django.command'](
+        SETTINGS, command, BIN_ENV, PYTHONPATH)
+
+def migrate():
+    command = "migrate"
+    SETTINGS = "autoroute.prod"
+    BIN_ENV = "/home/django/treadhub.com"
+    PYTHONPATH = "/home/django/treadhub.com/src/leadville/"
+
+    return __salt__['django.command'](
+        SETTINGS, command, BIN_ENV, PYTHONPATH)
+
+
+def collectstatic():
+    SETTINGS = "autoroute.prod"
+    BIN_ENV = "/home/django/treadhub.com"
+    PYTHONPATH = "/home/django/treadhub.com/src/leadville/"
+
+    return __salt__['django.collectstatic'](
+        SETTINGS, BIN_ENV, pythonpath=PYTHONPATH)
+
+def foo():
+    return True
