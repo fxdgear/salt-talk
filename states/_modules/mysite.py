e5a4d2f4728f5f9480f6e7ef6d04cc1c26eb0827
Nick Lang <nick@nicklang.com>
2013-03-19 22:39:10 -0600

    updating deploy command

--- states/_modules/mysite.py
+++ states/_modules/mysite.py
@@ -48,5 +48,5 @@ def quick_deploy():
 
 
 def full_deploy():
-    return backup_db(), quick_deploy()
+    return backup_db(), update_code(), migrate(), collectstatic(), restart_apache(), restart_supervisord()
 
